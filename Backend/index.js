5aab8ec8cf89e3d2b91c314f68f9bf416e21f109
Aryan Chakradhar <aryanchakradhar11@gmail.com>
2025-02-20 02:02:55 +0545

    Add required package for the backend User Authentication

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,32 @@
-const express = require("express")
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require('mongoose');
 const dotenv = require("dotenv");
-const cors = require("cors")
+const helmet = require("helmet");
+const cors = require("cors");
 const connectDB = require("./config/db");
+const bodyParser = require('body-parser');
+
+const userRoute = require("./routes/user");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express()
 dotenv.config();
 app.use(express.json())
+app.use(helmet());
 app.use(cors())
+app.use(bodyParser.json())
+
+//routes
+app.use("/api/user", userRoute);
+
+//error middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 //connect db and start server
 connectDB();
 
 
-app.listen(5000, () => {
-  console.log("listening on port 5000");
+app.listen(8080, () => {
+  console.log("listening on port 8080");
 });
