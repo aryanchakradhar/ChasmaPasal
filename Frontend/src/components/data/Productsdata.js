d14972771bf258f9b4e9f94bc2102cdca0f44770
Aryan Chakradhar <aryanchakradhar11@gmail.com>
2025-03-01 21:52:09 +0545

    Add dummy data for the product card

---
+++ Frontend/src/components/data/Productsdata.js
@@ -0,0 +1,31 @@
+// productsData.js
+export const Productsdata = [
+  {
+    _id: "1",
+    name: "Classic Aviators",
+    image: "./images/Lettori di Occhiali da sole Bifocali polarizzati Stile pilota Uomo.jpeg",
+    price: 120,
+    sku: "CA123"
+  },
+  {
+    _id: "2",
+    name: "Retro Round",
+    image: "./images/Lettori di Occhiali da sole Bifocali polarizzati Stile pilota Uomo.jpeg",
+    price: 80,
+    sku: "RR456"
+  },
+  {
+    _id: "3",
+    name: "Modern Square",
+    image: "./images/Lettori di Occhiali da sole Bifocali polarizzati Stile pilota Uomo.jpeg",
+    price: 150,
+    sku: "MS789"
+  },
+  {
+    _id: "4",
+    name: "Sport Shades",
+    image: "./images/Lettori di Occhiali da sole Bifocali polarizzati Stile pilota Uomo.jpeg",
+    price: 100,
+    sku: "SS101"
+  }
+];
