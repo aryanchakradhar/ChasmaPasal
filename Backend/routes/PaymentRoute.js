e159a218c336832a252e0ccf3dd160f68a314e48
Aryan Chakradhar <aryanchakradhar11@gmail.com>
2025-04-20 21:14:02 +0545

    Add online payment backend

---
+++ Backend/routes/PaymentRoute.js
@@ -0,0 +1,9 @@
+// const express = require('express')
+// const { initializeKhaltiPayment } = require('../controllers/khalti')
+// const router = express.Router()
+
+
+// router.post("/payment",initializeKhaltiPayment)
+
+
+// module.exports = router
